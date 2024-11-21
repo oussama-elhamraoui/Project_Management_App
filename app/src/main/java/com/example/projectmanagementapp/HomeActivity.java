b459be5084d59f1ce84f9b27a8878a7bba2680ca
YounesseAmhend <younesseamhend2019@gmail.com>
2024-11-21 15:21:50 +0100

    did some chanes

---
+++ app/src/main/java/com/example/projectmanagementapp/HomeActivity.java
@@ -0,0 +1,9 @@
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity{
+    
+}
