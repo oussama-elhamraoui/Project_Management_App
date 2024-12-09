e4a4e7144dee9b445b046786768b6ae9f050df13
youssefCOD <147263155+youssefCOD@users.noreply.github.com>
2024-12-09 01:30:20 +0100

    worked on the profile page and it's connectivity with the back end [problem with the extraction of the data form the jwt]

---
+++ app/src/main/java/com/example/projectmanagementapp/data/remote/model/UserProfile.java
@@ -0,0 +1,31 @@
+package com.example.projectmanagementapp.data.remote.model;
+
+public class UserProfile {
+    private final String userId;
+    private final String email;
+
+    private final String firstname;
+    private final String lastname;
+
+    public UserProfile(String userId, String email, String firstname,String lastname) {
+        this.userId = userId;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+}
