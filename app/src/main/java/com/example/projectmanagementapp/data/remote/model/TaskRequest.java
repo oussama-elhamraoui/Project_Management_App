df6a10a4bf8785e1e8bae1927b7b8f102b829642
Oussama Elhamraoui <oussama14elhamraoui@gmail.com>
2024-12-11 19:00:01 +0100

    adding to tasks backend

--- app/src/main/java/com/example/projectmanagementapp/data/remote/model/TaskRequest.java
+++ app/src/main/java/com/example/projectmanagementapp/data/remote/model/TaskRequest.java
@@ -1,4 +1,66 @@
 package com.example.projectmanagementapp.data.remote.model;
 
+import java.util.Date;
+
 public class TaskRequest {
+    private String title;
+    private String description;
+    private String priority; //set a default value if not to be used
+    private String status;
+    private Date dueDate;
+
+    public TaskRequest(String title, String description, String priority, String status, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public TaskRequest(String title, String description, String status, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
 }
--- app/src/main/java/com/example/projectmanagementapp/data/remote/repository/TaskRepository.java
+++ app/src/main/java/com/example/projectmanagementapp/data/remote/repository/TaskRepository.java
@@ -1,4 +1,11 @@
 package com.example.projectmanagementapp.data.remote.repository;
 
+import com.example.projectmanagementapp.data.remote.api.ApiClient;
+import com.example.projectmanagementapp.data.remote.api.ApiService;
+
 public class TaskRepository {
+    private ApiService taskApi;
+    public TaskRepository() {
+        taskApi = ApiClient.getInstance().create(ApiService.class);
+    }
 }
