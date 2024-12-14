8dba4d8d128f7f054c5c263f6c56f7b55989a8cc
YounesseAmhend <younesseamhend2019@gmail.com>
2024-12-14 21:03:10 +0100

    fixed some code for oussama

--- app/src/main/java/com/example/projectmanagementapp/data/remote/model/SignUpResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectmanagementapp.data.remote.model;
-
-public class SignUpResponse {
-    private String massage;
-
-    public String getMassage() {
-        return massage;
-    }
-}
